ae56c704835f2a10a9fd5fd27cdea1e3902ddcfb
Keika Jones <keika.jones@gmail.com>
2014-11-03 14:39:57 -0500

    fixed drop down form

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,32 +2,18 @@ class HomeController < ApplicationController
 
   def index
   	@emotions = Emotion.all
-  	# @emotion = ""
+    @emotions = @emotions.each
   end
 
   def show 
-  	@emotion = params[:emotion]
-  	if @emotion == "happy" #Emotion.find(params[:emotion])
-  		# puts "*"*50
-  		# puts "happy"
-  		# puts "*"*50
-  		@emotion = Emotion.find(1).type
-  		flash[:notice] = "the emotion is happy"
-  		redirect_to emotions_happy_path
-  	else 
-  		# puts "*"*50
-  		# puts @emotion
-  		# puts "*"*50
-  		@emotion = Emotion.find(2).type
-  		flash[:alert] = "the emotion is sad"
-  		redirect_to emotions_sad_path
-  	end
+  	@emotion = Emotion.find(params[:name])
   end
+
 
   private
 
-  def feeling_params
-  	params.require(:emotion).permit(:emotion)
+  def emotion_params
+  	params.require(:emotion).permit(:name)
   end
 
   def set_feeling

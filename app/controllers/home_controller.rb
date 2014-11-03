76c36f05d10687a44fe3eb841801bd393aa4f51f
Jason <yimjason@aol.com>
2014-11-03 15:09:07 -0500

    merge

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,28 +4,9 @@ class HomeController < ApplicationController
   	@emotions = Emotion.all 
   end
 
-  def show 
-  	@emotion = Emotion.find(feeling_params)
-  	if @emotion == "happy" 
-  		@emotion = Emotion.find(1).type
-  		flash[:notice] = "the emotion is happy"
-  		redirect_to emotions_happy_path
-  	else  
-  		@emotion = Emotion.find(2).type
-  		flash[:alert] = "the emotion is sad"
-  		redirect_to emotions_sad_path
-  	end
+  def show   
   end
 
-  private
-
-  def feeling_params
-  	params.require(:emotion).permit(:type)
-  end
-
-  def set_feeling
-  	@emotion = Emotion.find(params[:id])
-  end
-
-
+  private 
+  
 end

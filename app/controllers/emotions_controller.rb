119e8f521b41256c522dc74c1c2e4ea62150e1fb
nyhunter77 <nyhunter77@yahoo.com>
2014-11-02 20:44:55 -0500

    added picture / video / color change on One view

--- app/controllers/emotions_controller.rb
+++ app/controllers/emotions_controller.rb
@@ -1,40 +1,14 @@
 class EmotionsController < ApplicationController
 
 	def any_emotion
-
+		@feeling = params[:feeling]
+		if @feeling == "happy"
+			puts "happy"
+			#redirect_to emotions_any_emotion_path
+		else
+			puts "sad"
+			#redirect_to emotions_any_emotion_path
+		end
 	end
-
-	
-	# before_action :set_feeling, only: [:create]
-
-	# def index 
-	# 	@feelings=Emotion.all
-	# end
-
-	# def new
-	# 	@feeling=Emotion.new
-	# end
-
-	# def update
-	# 	@feeling = Emotion.update(feeling_params)
-	# 	if @feeling.update
-	# 		flash[:notice] = "Emotion updated"
-	# 		redirect_to 'emotion_path'
-	# 	else
-	# 		flash[:alert] = "Something went wrong."
-	# 		redirect_to :new
-	# 	end
-
-	# end
-
-	# private
-
-	# def feeling_params
-	# 	params.require(:emotion).permit(:feeling)
-	# end
-
-	# def set_feeling
-	# 	@feeling = Emotion.find(params[:id])
-	# end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get 'emotions/happy'
   get 'emotions/sad'
   get 'emotions/any_emotion'
+  post 'emotions/any_emotion'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
